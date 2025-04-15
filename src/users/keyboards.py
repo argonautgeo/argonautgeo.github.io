59b8bd5e4b5ebf92f9c97a5f80bc2f16fdac635a
argonautgeo <barsik030@bk.ru>
2025-04-15 22:17:19 +0800

    Update keyboards.py

--- src/users/keyboards.py
+++ src/users/keyboards.py
@@ -7,9 +7,6 @@ main_kb = [
     [KeyboardButton(text="Сделать заказ", web_app=WebAppInfo(url=conf.webapp_url))]
 ]
 
-# Передать информацию из сайта в тг можно только через Reply-клавиатуру,
-# но при этом сайт не видит наших данных. В Inline-клавиатуре всё наоборот.
-# Почему и зачем тг так сделали - одному Богу известно.
 reply_main_kb = ReplyKeyboardMarkup(
     keyboard=main_kb,
     # Чтобы прилично выглядеть на телефоне.
