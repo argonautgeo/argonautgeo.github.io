7f70f345bf6d567a31a88db7d2113632286e4ae7
argonautgeo <barsik030@bk.ru>
2025-04-15 21:12:49 +0800

    Update handlers.py

--- src/payments/handlers.py
+++ src/payments/handlers.py
@@ -4,71 +4,90 @@ from aiogram.types import (
     LabeledPrice,
     ContentType,
     PreCheckoutQuery,
+    SuccessfulPayment,
     ShippingQuery,
 )
 
 from src.config import conf
-from src.db.dependencies import db
-from src.payments import constants as constants
+from src.payments.constants import (
+    PRODUCTS_PRICES,
+    VALID_CITIES,
+    SHIPPING_OPTIONS,
+    WRONG_PRODUCT_INFO_MSG,
+    NO_DELIVERY_MSG,
+    ORDER_ACCEPTED_MSG,
+)
 
 router = Router()
 
+# Определение фиксированных продуктов
+PRODUCTS_PRICES = {
+    '1': {"name": "Продукт 1", "amount": 220},
+    '2': {"name": "Продукт 2", "amount": 460},
+    '3': {"name": "Продукт 3", "amount": 240},
+    '4': {"name": "Продукт 4", "amount": 190},
+    '5': {"name": "Продукт 5", "amount": 100},
+    '6': {"name": "Продукт 6", "amount": 687},
+    '7': {"name": "Продукт 7", "amount": 800},
+    '8': {"name": "Продукт 8", "amount": 135},
+    '9': {"name": "Продукт 9", "amount": 550},
+    '10': {"name": "Продукт 10", "amount": 736},
+    '11': {"name": "Продукт 11", "amount": 999},
+    '12': {"name": "Продукт 12", "amount": 1270},
+}
 
+# Хэндлер для начала процесса покупки
 @router.message(F.content_type == ContentType.WEB_APP_DATA)
-async def buy_process(msg: Message) -> None:
-    prod_id = msg.web_app_data.data
-    if prod_id.isnumeric():
-        prod = await db.get_product(int(prod_id))
-        if prod:
-            price = [LabeledPrice(label=prod["name"], amount=prod["amount"] * 100)]
-        else:
-            await msg.answer(constants.WRONG_PRODUCT_INFO_MSG)
-            return
+async def buy_process(message: Message) -> None:
+    product_id = message.web_app_data.data
+    if product_id.isnumeric() and product_id in PRODUCTS_PRICES.keys():
+        product = PRODUCTS_PRICES[product_id]
+        price = [LabeledPrice(label=product["name"], amount=int(float(product["amount"]) * 100))]
     else:
-        await msg.answer(constants.WRONG_PRODUCT_INFO_MSG)
+        await message.answer(WRONG_PRODUCT_INFO_MSG)
         return
 
-    await msg.answer_invoice(
-        title="Оплата заказа",
-        description=price[0].label,
+    await message.answer_invoice(
+        title=f"{product['name']} ({product['amount']} руб.)",
+        description=product["name"],
         provider_token=conf.pay_token,
         currency="rub",
         need_email=True,
         need_phone_number=True,
         prices=price,
-        start_parameter="example",
-        payload="some_invoice",
+        start_parameter="buy_product",
+        payload="some_invoice_payload",
         is_flexible=True,
     )
 
-
-def check_validity(query: ShippingQuery) -> bool:
+# Функция проверки доставки
+def check_delivery_validity(query: ShippingQuery) -> bool:
     return (
-            query.shipping_address.country_code == "RU"
-            and query.shipping_address.city.lower() in constants.VALID_CITIES
+        query.shipping_address.country_code.upper() == "RU"
+        and query.shipping_address.city.lower() in VALID_CITIES
     )
 
-
-@router.shipping_query(lambda query: True)
+# Хэндлер для обработки вопросов о доставке
+@router.shipping_query()
 async def shipping_process(query: ShippingQuery) -> None:
-    if not check_validity(query):
+    if not check_delivery_validity(query):
         await query.answer(
             ok=False,
-            error_message=constants.NO_DELIVERY_MSG.format(query.shipping_address.city),
+            error_message=NO_DELIVERY_MSG.format(query.shipping_address.city),
         )
         return
 
     await query.answer(
         ok=True,
-        shipping_options=constants.SHIPPING_OPTIONS,
+        shipping_options=SHIPPING_OPTIONS,
     )
 
+# Хэндлер подтверждения платежа
+@router.pre_checkout_query()
+async def pre_checkout_process(query: PreCheckoutQuery) -> None:
+    await query.answer(ok=True)
 
-@router.pre_checkout_query(lambda query: True)
-async def pre_checkout_process(pre_checkout: PreCheckoutQuery) -> None:
-    await pre_checkout.answer(ok=True)
-
-
+# Хэндлер успешной оплаты
 @router.message(F.content_type == ContentType.SUCCESSFUL_PAYMENT)
-async def successful_payment(msg: Message) -> None:
-    await msg.answer(constants.ORDER_ACCEPTED_MSG)
+async def successful_payment(message: Message) -> None:
+    await message.answer(ORDER_ACCEPTED_MSG)
