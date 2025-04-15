7b664f18e0c7702c44a5de22c19f6423d1b8b522
argonautgeo <barsik030@bk.ru>
2025-04-15 18:05:19 +0800

    Update keyboards.py

--- src/users/keyboards.py
+++ src/users/keyboards.py
@@ -4,15 +4,12 @@ from aiogram.types.web_app_info import WebAppInfo
 from src.config import conf
 
 main_kb = [
-    [KeyboardButton(text="Сделать заказ", web_app=WebAppInfo(url=conf.webapp_url))]
+    [KeyboardButton(text="Сделать заказ", web_app=WebAppInfo(url="https://argonautgeo.github.io/"))]
 ]
 
-# Передать информацию из сайта в тг можно только через Reply-клавиатуру,
-# но при этом сайт не видит наших данных. В Inline-клавиатуре всё наоборот.
-# Почему и зачем тг так сделали - одному Богу известно.
 reply_main_kb = ReplyKeyboardMarkup(
     keyboard=main_kb,
-    # Чтобы прилично выглядеть на телефоне.
+    
     resize_keyboard=True,
     input_field_placeholder="Выберите пункт меню",
 )
