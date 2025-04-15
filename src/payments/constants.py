c872c4846bb3f01c831b3b2f91fc4d2ec009ba9a
argonautgeo <barsik030@bk.ru>
2025-04-15 22:17:38 +0800

    Update constants.py

--- src/payments/constants.py
+++ src/payments/constants.py
@@ -1,19 +1,19 @@
 from aiogram.types import LabeledPrice, ShippingOption
 
 ORDER_ACCEPTED_MSG = (
-    """Заказ принят в обработку. \nОжидайте звонка от нас. \n\nТвой DarkSide 🖤"""
+    """Заказ принят в обработку. \nОжидайте звонка от нас. \n\nВаш GEOSHOP🖤"""
 )
 WRONG_PRODUCT_INFO_MSG = """Неправильный формат информации о товаре 😒"""
 NO_DELIVERY_MSG = """Доставка в город {} не осуществляется 😒"""
 VALID_CITIES = {
     "нижний новгород",
-    "арзамас",
+    "иркутск",
     "санкт-петербург",
     "сочи",
     "москва",
-    "бор",
-    "дзержинск",
-    "киров",
+    "зима",
+    "братск",
+    "владивосток",
 }
 SHIPPING_PRICES = {
     "regular": [LabeledPrice(label="Обычная доставка", amount=0 * 100)],
@@ -21,7 +21,7 @@ SHIPPING_PRICES = {
 }
 SHIPPING_OPTIONS = [
     ShippingOption(
-        id="regular", title="Обычная (1-2 ч)", prices=SHIPPING_PRICES["regular"]
+        id="regular", title="Обычная (1-2 дня)", prices=SHIPPING_PRICES["regular"]
     ),
-    ShippingOption(id="fast", title="Быстрая (45 мин)", prices=SHIPPING_PRICES["fast"]),
+    ShippingOption(id="fast", title="Быстрая (12 ч)", prices=SHIPPING_PRICES["fast"]),
 ]
