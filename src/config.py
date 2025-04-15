38601cd1fb9ed7ee4ea894cdb2de7593d4c9d1e4
argonautgeo <barsik030@bk.ru>
2025-04-15 18:27:01 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
-    bot_token: "7930467714:AAGnJixu-zm1bpe3MK90o4DeeALRHiTQXsM"
-    pay_token: "1744374395:TEST:577bcbcc61a58ae0ad9a"
-    webapp_url: "https://argonautgeo.github.io/"
+    bot_token: str
+    pay_token: str
+    webapp_url: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
